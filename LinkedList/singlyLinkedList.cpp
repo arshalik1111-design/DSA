724cfc8841f093cfb7880b0a6e6baf56a4eb6889
Arsh Ali Khan <arshalik1111@gmail.com>
2026-07-04 10:42:36 +0530

    Striver's A2Z Sheet

--- LinkedList/singlyLinkedList.cpp
+++ LinkedList/singlyLinkedList.cpp
@@ -783,6 +783,19 @@ public:
         }
         return nullptr;
     }
+
+    Node *getIntersectionNodeOptimal2(Node *headA, Node *headB)
+    {
+        Node *t1 = headA;
+        Node *t2 = headB;
+        while (t1 != t2)
+        {
+            t1 = t1 == nullptr ? headB : t1->next;
+            t2 = t2 == nullptr ? headA : t2->next;
+        }
+        return t1;
+    }
+
     // Function to print the list
     void printList(Node *head)
     {
