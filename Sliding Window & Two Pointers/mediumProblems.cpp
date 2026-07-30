d6320b8906d9f024900079b319eccab3dc2b0eaf
Arsh Ali Khan <arshalik1111@gmail.com>
2026-07-30 21:33:25 +0530

    Striver's A2Z Sheet

--- Sliding Window & Two Pointers/mediumProblems.cpp
+++ Sliding Window & Two Pointers/mediumProblems.cpp
@@ -22,6 +22,30 @@ public:
         }
         return maxLength;
     }
+
+    // Leetcode 1004. Max Consecutive Ones III
+    int MaxConsecutiveOnesIII(vector<int> &nums, int k)
+    {
+        int n = nums.size();
+        int maxLen = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int zeroes = 0;
+            for (int j = i; j < n; j++)
+            {
+                if (nums[j] == 0)
+                {
+                    zeroes++;
+                }
+                if (zeroes > k)
+                {
+                    break;
+                }
+                maxLen = max(maxLen, j - i + 1);
+            }
+        }
+        return maxLen;
+    }
 };
 
 class Solution
@@ -46,13 +70,41 @@ public:
         }
         return maxLength;
     }
+
+    int MaxConsecutiveOnesIII(vector<int> &nums, int k)
+    {
+        int n = nums.size();
+        int maxLen = 0;
+        int zeroes = 0;
+        int left = 0;
+        for (int right = 0; right < n; right++)
+        {
+            if (nums[right] == 0)
+            {
+                zeroes++;
+            }
+            while (zeroes > k)
+            {
+                if (nums[left] == 0)
+                {
+                    zeroes--;
+                }
+                left++;
+            }
+
+            maxLen = max(maxLen, right - left + 1);
+        }
+        return maxLen;
+    }
 };
 
 int main()
 {
 
-    string s = "abcddabac";
+    // string s = "abcddabac";
+    vector<int> nums = {1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0};
+    int k = 3;
     Solution sbf;
-    int r = sbf.lengthOfLongestSubstring(s);
+    int r = sbf.MaxConsecutiveOnesIII(nums, k);
     cout << r;
 }
